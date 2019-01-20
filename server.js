99f1a7cfa44fbaacf5b83c9499110391932c2b43
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2019-01-19 17:55:35 -0800

    changes to PORT

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -149,5 +149,5 @@ app.post("/articles/:id", function(req, res) {
 
 // Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "! \n http://localhost:3000");
+  console.log("App running on port " + PORT);
 });
